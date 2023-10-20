25573efa482b0986f9508b1869a9ae6af89136d0
yaoleksa <mryaremchyk@gmail.com>
2023-10-20 20:47:03 +0300

    improve logic

--- 01_cli_interactive_sort/app.js
+++ 01_cli_interactive_sort/app.js
@@ -4,14 +4,13 @@ const readline = require('readline').createInterface({
 });
 
 const arr = [];
-let exception = false;
 
 const askInput = () => {
   readline.question('Hello! Enter 10 words or digits deviding them in spaces: ', input => {
     const convertedToArray = input.split(' ');
     if(convertedToArray.length != 10) {
-      exception = true;
-      throw new Error('You must input exactly 10 words or digits');
+      console.log('You must enter exactly ten words or numbers');
+      askInput();
     } else {
       arr.push(...convertedToArray);
     }
@@ -22,10 +21,12 @@ const askInput = () => {
       4. Display words in ascending order by number of letters in the word
       5. Show only unique words
       6. Display only unique values from the set of words and numbers entered by the user
-      7. To exit the program, the user need to enter exit, otherwise the program will repeat itself again and again, asking for new data and suggesting sorting`, sortType => {
-        if(sortType == 'exit') {
-          readline.close();
-          return;
+      To exit the program, the user need to enter exit, otherwise the program will repeat itself again and again, asking for new data and suggesting sorting`, sortType => {
+        const trimmed = sortType.trim();
+        switch(trimmed) {
+          case 'exit':
+            readline.close();
+            return;
         }
         askInput();
       });
