d3c9d4eb8152b46849ae6c40f4b8d48f3486ba0a
yaoleksa <mryaremchyk@gmail.com>
2023-10-21 19:46:12 +0300

    beta release

--- 01_cli_interactive_sort/app.js
+++ 01_cli_interactive_sort/app.js
@@ -3,9 +3,28 @@ const readline = require('readline').createInterface({
   output: process.stdout,
 });
 
-const arr = [];
+const allWords = (list) => {
+  let result = true;
+  list.forEach(e => {
+    if(!Number.isNaN(Number(e))) {
+      result = false;
+    }
+  });
+  return result;
+}
+
+const allNumbers = (list) => {
+  let result = true;
+  list.forEach(e => {
+    if(Number.isNaN(e)) {
+      result = false;
+    }
+  });
+  return result;
+}
 
 const askInput = () => {
+  let arr = [];
   readline.question('Hello! Enter 10 words or digits deviding them in spaces: ', input => {
     const convertedToArray = input.split(' ');
     if(convertedToArray.length != 10) {
@@ -21,23 +40,84 @@ const askInput = () => {
       4. Display words in ascending order by number of letters in the word
       5. Show only unique words
       6. Display only unique values from the set of words and numbers entered by the user
-      To exit the program, the user need to enter exit, otherwise the program will repeat itself again and again, asking for new data and suggesting sorting`, sortType => {
+      To exit the program, the user need to enter exit, otherwise the program will repeat itself again and again, asking for new data and suggesting sorting   `, sortType => {
         const trimmed = sortType.trim();
         switch(trimmed) {
           case '1':
-            readline.close();
-            console.log(arr.sort());
-            return;
+            if(allWords(arr)) {
+              console.log(arr.sort());
+            } else {
+              console.log('For this type of sorting your must contain only words');
+              askInput();
+            }
+            break;
           case '2':
-            return checkType(arr);
+            if(allNumbers) {
+              arr.sort((a, b) => {
+                if(a < b) {
+                  return -1;
+                } else if(a > b) {
+                  return 1;
+                }
+                return 0;
+              });
+              console.log(arr);
+            } else {
+              console.log('For this type of sorting your must contain only numbers');
+              askInput();
+            }
+            break;
           case '3':
-            return checkType(arr);
+            if(allNumbers) {
+              arr.sort((a, b) => {
+                if(a < b) {
+                  return 1;
+                } else if(a > b) {
+                  return -1;
+                }
+                return 0;
+              });
+              console.log(arr);
+            } else {
+              console.log('For this type of sorting your must contain only numbers');
+              askInput();
+            }
+            break;
           case '4':
-            return checkType(arr);
+            if(allWords(arr)) {
+              arr.sort((a, b) => {
+                if(a.length < b.length) {
+                  return -1;
+                } else if(a.length > b.length) {
+                  return 1;
+                }
+                return 0;
+              });
+              console.log(arr);
+            } else {
+              console.log('For this type of sorting your must contain only words');
+              askInput();
+            }
+            break;
           case '5':
-            return checkType(arr);
+            if(allWords(arr)) {
+              const unique = new Set();
+              arr.forEach(e => {
+                unique.add(e);
+              });
+              console.log(unique);
+            } else {
+              console.log('For this type of sorting your must contain only words');
+              askInput();
+            }
+            break;
           case '6':
-            return checkType(arr);
+            const unique = new Set();
+            arr.forEach(e => {
+              unique.add(e);
+            });
+            console.log(unique);
+            break;
           case 'exit':
             readline.close();
             return;
