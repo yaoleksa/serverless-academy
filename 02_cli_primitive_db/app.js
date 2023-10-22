a490feba1b4127c02790315d34b3a5c4b5c000ef
yaoleksa <mryaremchyk@gmail.com>
2023-10-22 15:37:24 +0300

    continue

--- 02_cli_primitive_db/app.js
+++ 02_cli_primitive_db/app.js
@@ -2,13 +2,30 @@ import inquirer from "inquirer";
 
 class Question {
     name;
+    type;
+    message;
+    choices;
     constructor(params) {
         this.name = params.name;
+        this.type = params.type;
+        this.message = params.message;
+        this.choices = params.choices;
     }
 }
 
-const userName = new Question({"name": "Enter user\'s name. To cancel press ENTER: "});
+const userName = new Question({
+    name: "NameOfTheUser", 
+    type: "input",
+    message: "Enter user\'s name. To cancel press ENTER: ",
+});
 
-inquirer.prompt([userName]).then(answer => {
+const userGender = new Question({
+    name: "GenderOfTheUser",
+    type: "list",
+    choices: ['Male', 'Female'],
+    message: "Choose your gender. To cancel press ENTER: ",
+});
+
+inquirer.prompt([userName, userGender]).then(answer => {
     console.log(answer);
 })
