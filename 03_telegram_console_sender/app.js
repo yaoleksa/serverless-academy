ea8c7fda6ac91ee5996ab80707db706fc2720ee7
yaoleksa <mryaremchyk@gmail.com>
2023-10-23 18:27:23 +0300

    bot is alive

--- 03_telegram_console_sender/app.js
+++ 03_telegram_console_sender/app.js
@@ -1,13 +1,13 @@
 const { Command } = require('commander');
 const TelegramBot = require('node-telegram-bot-api');
 const program = new Command();
-const Token = "MY_TELEGRAM_TOKEN";
-//const bot = new TelegramBot(Token, {polling: true});
-const chatId = "MY_TELEGRAM_BOT_CHAT_ID";
+const Token = "6587401436:AAEcW-YleFJz8Dum4ufoBitiHMdczRhmlY0";
+const bot = new TelegramBot(Token, {polling: true});
+const chatId = "2024237143";
 
 program.name('Text sender').description('Send text message to Telegram bot').version('0.0.0');
 program.command('send-txt').description('Send entered text to Telegram bot').argument('<string>', 'text to send').action((input) => {
-    //bot.sendMessage(chatId, input);
+    bot.sendMessage(chatId, input);
     console.log(`Here we are: ${input}`);
 });
 
