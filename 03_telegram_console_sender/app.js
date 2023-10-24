b4d564066468b2eb188427e4f90c4fde104e8dbb
yaoleksa <mryaremchyk@gmail.com>
2023-10-24 11:51:35 +0300

    Add alias

--- 03_telegram_console_sender/app.js
+++ 03_telegram_console_sender/app.js
@@ -6,14 +6,13 @@ const bot = new TelegramBot(Token, {polling: true});
 const chatId = "2024237143";
 
 program.name('./app.js').description('Send message to Telegram bot').version('0.0.0');
-program.command('message').description('Send entered text to Telegram bot').argument('<string>', 'text to send').action((input) => {
+program.command('message').alias('m').description('Send entered text to Telegram bot').argument('<string>', 'text to send').action((input) => {
     bot.sendMessage(chatId, input);
     console.log(`Message ${input} was successfully sent`);
 });
-program.command('m').description('Send entered text to Telegram bot').argument('<string>', 'text to send').action((input) => {
-    bot.sendMessage(chatId, input);
-    console.log(`Message ${input} was successfully sent`);
+program.command('photo').alias('p').description('Send photo to Telegram bot').argument('<string>', 'path to photo').action((path) => {
+    bot.sendPhoto(chatId, path);
+    console.log('Photo successfully sent to Telegram');
 });
-//program
 
 program.parse();
