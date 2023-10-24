59922e2796053bc2333a808b2ae06259bf0553b9
yaoleksa <mryaremchyk@gmail.com>
2023-10-24 16:40:43 +0300

    add README

--- 03_telegram_console_sender/app.js
+++ 03_telegram_console_sender/app.js
@@ -1,18 +1,22 @@
 const { Command } = require('commander');
 const TelegramBot = require('node-telegram-bot-api');
 const program = new Command();
-const Token = "6587401436:AAEcW-YleFJz8Dum4ufoBitiHMdczRhmlY0";
-const bot = new TelegramBot(Token, {polling: true});
-const chatId = "2024237143";
+const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 
 program.name('./app.js').description('Send message to Telegram bot').version('0.0.0');
 program.command('message').alias('m').description('Send entered text to Telegram bot').argument('<string>', 'text to send').action((input) => {
-    bot.sendMessage(chatId, input);
+    bot.sendMessage(process.env.CHAT_ID, input);
     console.log(`Message ${input} was successfully sent`);
+    setTimeout(() => {
+        process.exit();
+    }, 5000);
 });
-program.command('photo').alias('p').description('Send photo to Telegram bot').argument('<string>', 'path to photo').action((path) => {
-    bot.sendPhoto(chatId, path);
+program.command('photo').alias('p').description('Send photo to Telegram bot').argument('<path-to-file>', 'path to photo').action((path) => {
+    bot.sendPhoto(process.env.CHAT_ID, path);
     console.log('Photo successfully sent to Telegram');
+    setTimeout(() => {
+        process.exit();
+    }, 10000);
 });
 
 program.parse();
