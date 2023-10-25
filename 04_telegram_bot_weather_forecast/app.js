5fddbfc8f178265274e773416ae1fb2b16a9d628
yaoleksa <mryaremchyk@gmail.com>
2023-10-25 12:43:59 +0300

    adding API logic

--- 04_telegram_bot_weather_forecast/app.js
+++ 04_telegram_bot_weather_forecast/app.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 
-bot.onText(/\/start/, (msg) => {
+bot.onText(/\/start/, msg => {
     const opts = {
         reply_to_message_id: msg.message_id,
         reply_markup: JSON.stringify({
@@ -14,7 +14,7 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Hello!', opts);
 });
 
-bot.onText(/Forecast in Nice/, (msg) => {
+bot.onText(/Forecast in Nice/, msg => {
     const opts = {
         reply_to_message_id: msg.message_id,
         reply_markup: JSON.stringify({
@@ -25,4 +25,25 @@ bot.onText(/Forecast in Nice/, (msg) => {
         })
     };
     bot.sendMessage(msg.chat.id, 'Select interval', opts);
+});
+
+bot.onText(/at intervals of 3 hours/, msg => {
+    weatherForecastRequest(msg.chat.id);
+    setInterval(weatherForecastRequest, 1000 * 60 * 180, msg.chat.id);
+});
+
+bot.onText(/at intervals of 6 hours/, msg => {
+    weatherForecastRequest();
 })
+
+function weatherForecastRequest(chatId) {
+    axios.get('https://pro.openweathermap.org/data/2.5/forecast/hourly', {
+        params: {
+            q: `Lviv,UA`,
+            appid: process.env.APPID
+        }
+    }).then(res => {
+        console.log(res.data);
+        bot.sendMessage(chatId, res.data);
+    })
+}
