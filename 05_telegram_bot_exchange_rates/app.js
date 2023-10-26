c75711fd8690ccbfedffe1d1ff6bf7163dc247a0
yaoleksa <mryaremchyk@gmail.com>
2023-10-26 14:06:02 +0300

    finish

--- 05_telegram_bot_exchange_rates/app.js
+++ 05_telegram_bot_exchange_rates/app.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 const nodeCache = require('node-cache');
+const cache = new nodeCache();
 const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 
 bot.onText(/\/start/, msg => {
@@ -17,27 +18,47 @@ bot.onText(/\/start/, msg => {
 });
 
 bot.onText(/USD/, msg => {
-    axios.get('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11').then(res => {
-        const result = {};
-        res.data.forEach(e => {
-            if(e.ccy == 'USD') {
-                result["buy"] = e.buy;
-                result["sale"] = e.sale;
-            }
+    try {
+        axios.get('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11').then(res => {
+            const result = {};
+            res.data.forEach(e => {
+                if(e.ccy == 'USD') {
+                    result["buy"] = e.buy;
+                    result["sale"] = e.sale;
+                    cache.set(e.ccy, e);
+                }
+            });
+            bot.sendMessage(msg.chat.id, JSON.stringify(result));
         });
+    } catch(Exception) {
+        console.error(Exception.message);
+        const result = {};
+        const cahed = chache.get('USD');
+        result["buy"] = cahed.buy;
+        result["sale"] = cahed.sale;
         bot.sendMessage(msg.chat.id, JSON.stringify(result));
-    });
+    }
 });
 
 bot.onText(/EUR/, msg => {
-    axios.get('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11').then(res => {
-        const result = {};
-        res.data.forEach(e => {
-            if(e.ccy == 'EUR') {
-                result["buy"] = e.buy;
-                result["sale"] = e.sale;
-            }
+    try {
+        axios.get('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11').then(res => {
+            const result = {};
+            res.data.forEach(e => {
+                if(e.ccy == 'EUR') {
+                    result["buy"] = e.buy;
+                    result["sale"] = e.sale;
+                    cache.set(e.ccy, e);
+                }
+            });
+            bot.sendMessage(msg.chat.id, JSON.stringify(result));
         });
+    } catch(Exception) {
+        console.error(Exception.message);
+        const result = {};
+        const cached = cache.get('EUR');
+        result["buy"] = cached.buy;
+        result["sale"] = cached.sale;
         bot.sendMessage(msg.chat.id, JSON.stringify(result));
-    });
+    }
 });
