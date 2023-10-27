b5be62776905c977e5bee46b345f4f6962d04fce
yaoleksa <mryaremchyk@gmail.com>
2023-10-27 23:17:26 +0300

    almost final

--- 07_grouping_vacations/app.js
+++ 07_grouping_vacations/app.js
@@ -5,11 +5,31 @@ fs.readFile('./data.json', (err, data) => {
     }
     if(data) {
         const convertedData = data.toString('utf-8');
-        const beforeParsing = convertedData.slice(convertedData.indexOf('[') + 1, convertedData.lastIndexOf(']')).replace(/\},\r\n\s+\{/g, '}*\n{').split("*\n");
-        const convertedToArray = [];
+        const beforeParsing = convertedData.slice(convertedData.indexOf('[') + 1, convertedData
+        .lastIndexOf(']')).replace(/\},\r\n\s+\{/g, '}*\n{').split("*\n");
+        const unique = new Map();
+        let vacation;
+        let user;
         beforeParsing.forEach(e => {
-            convertedToArray.push(JSON.parse(e));
+            vacation = JSON.parse(e);
+            if(!unique.has(vacation.user._id)) {
+                user = new User(vacation.user._id, vacation.user.name, vacation);
+                unique.set(user.userId, user);
+            } else {
+                unique.get(vacation.user._id).vacations.push(vacation);
+            }
         });
-        console.log(convertedToArray.length);
+        console.log(unique.size);
     }
-})
+});
+
+class User {
+    userId;
+    userName;
+    vacations;
+    constructor(user_id, user_name, vacation) {
+        this.userId = user_id;
+        this.userName = user_name;
+        this.vacations = [vacation];
+    }
+}
