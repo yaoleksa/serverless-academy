1c70627746b04d36992a5f1e59c39394ad9568c0
yaoleksa <mryaremchyk@gmail.com>
2023-10-30 10:21:05 +0200

    improve

--- 08_json_sorting/json-sorting.js
+++ 08_json_sorting/json-sorting.js
@@ -18,39 +18,18 @@ https://jsonbase.com/sls-team/json-79
 https://jsonbase.com/sls-team/json-706
 https://jsonbase.com/sls-team/json-521
 https://jsonbase.com/sls-team/json-350
-https://jsonbase.com/sls-team/json-64`.split('\n');
-
-let options;
-
-// endpoints.forEach(endpoint => {
-//     options = {
-//         host: endpoint.slice(endpoint.indexOf('/') + 2, endpoint.indexOf('.com/') + 4),
-//         port: 3000,
-//         path: endpoint.slice(endpoint.indexOf('.com/') + 4)
-//     }
-//     try {
-//         https.get(options, res => {
-//             console.log(res);
-//         });
-//     } catch(err) {
-//         console.log(err.message);
-//     }
-// });
-
-options = {
-    host: 'api.publicapis.org',
-    port: 3000,
-    path: '/entries'
-}
-
-// just check if it works at all
-
-https.get('https://api.publicapis.org/entries', res => {
-    const data = [];
-    res.on('data', chunk => {
-        data.push(chunk);
-    });
-    res.on('end', () => {
-        console.log(JSON.stringify(JSON.parse(Buffer.concat(data).toString()).entries, null, 2));
-    });
+https://jsonbase.com/sls-team/json-64`.split('\n').forEach(endpoint => {
+    try {
+        https.get(endpoint, res => {
+            const data = [];
+            res.on('data', chunk => {
+                data.push(chunk);
+            });
+            res.on('end', () => {
+                console.log(JSON.parse(Buffer.concat(data).toString()));
+            }).on('error', err => {
+                console.log('oh :(');
+            });
+        });
+    } catch(exception) {}
 });
