25591c1cc558fee2fe8bd843646b87ff8abb8025
yaoleksa <mryaremchyk@gmail.com>
2023-10-31 14:45:59 +0200

    try to do a call to my server

--- 08_json_sorting/json-sorting.js
+++ 08_json_sorting/json-sorting.js
@@ -1,24 +1,16 @@
-const { request } = require('http');
-const https = require('https');
-const endpoints = `https://jsonbase.com/sls-team/json-793
-https://jsonbase.com/sls-team/json-955
-https://jsonbase.com/sls-team/json-231
-https://jsonbase.com/sls-team/json-931
-https://jsonbase.com/sls-team/json-93
-https://jsonbase.com/sls-team/json-342
-https://jsonbase.com/sls-team/json-770
-https://jsonbase.com/sls-team/json-491
-https://jsonbase.com/sls-team/json-281
-https://jsonbase.com/sls-team/json-718
-https://jsonbase.com/sls-team/json-310
-https://jsonbase.com/sls-team/json-806
-https://jsonbase.com/sls-team/json-469
-https://jsonbase.com/sls-team/json-258
-https://jsonbase.com/sls-team/json-516
-https://jsonbase.com/sls-team/json-79
-https://jsonbase.com/sls-team/json-706
-https://jsonbase.com/sls-team/json-521
-https://jsonbase.com/sls-team/json-350
-https://jsonbase.com/sls-team/json-64`.split('\n');
+const http = require('http');
+const port = process.env.PORT || 3001;
+const endpoints = [];
+for(let i = 1; i < 21; i++) {
+    endpoints.push(`http://localhost:${port}/json${i}`);
+}
 
-console.log(endpoints.length);
+http.get(endpoints[11], res => {
+    const data = [];
+    res.on('data', chunk => {
+        data.push(chunk);
+    });
+    res.on('end', () => {
+        console.log(JSON.stringify(JSON.parse(Buffer.concat(data).toString()), null, 2));
+    })
+});
